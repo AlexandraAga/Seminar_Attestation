40e845fa3ca7f4868f2a606a7bc2edf719416aba
sasha <bananov.sasha.gmail.com>
2023-11-22 17:38:57 +0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,59 +1,59 @@
 ﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
 //Выполнить с помощью рекурсии.
 
-// int n = InputNumbers("Введите N: ");
-// int count = 2;
-// PrintNumber(n, count);
-// Console.Write(1);
+int n = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
 
-// void PrintNumber(int n, int count)
-// {
-//     if (count > n) return;
-//     PrintNumber(n, count + 1);
-//     Console.Write(count + ", ");
-// }
+void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Console.Write(count + ", ");
+}
 
-// int InputNumbers(string input)
-// {
-//     Console.Write(input);
-//     int output = Convert.ToInt32(Console.ReadLine());
-//     return output;
-// }
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
 
 //Задача 66: Задайте значения M и N. 
 //Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
 
-// int m = InputNumbers("Введите M: ");
-// int n = InputNumbers("Введите N: ");
-// int temp = m;
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int temp = m;
 
-// if (m > n)
-// {
-//     m = n;
-//     n = temp;
-// }
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
 
-// PrintSumm(m, n, temp = 0);
+PrintSumm(m, n, temp = 0);
 
-// void PrintSumm(int m, int n, int summ)
-// {
-//     summ = summ + n;
-//     if (n <= m)
-//     {
-//         Console.Write($"Сумма элементов = {summ} ");
-//         return;
-//     }
-//     PrintSumm(m, n - 1, summ);
-// }
+void PrintSumm(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {summ} ");
+        return;
+    }
+    PrintSumm(m, n - 1, summ);
+}
 
-// int InputNumbers(string input)
-// {
-//     Console.Write(input);
-//     int output = Convert.ToInt32(Console.ReadLine());
-//     return output;
-// }
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
  //Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
